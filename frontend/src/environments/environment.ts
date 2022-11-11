2e67fd8341ef91f10b2e5eadcd289ec55398f7a5
kritibh99 <kriti.bhargava@sourcefuse.com>
2022-11-11 15:16:52 +0530

    First commit

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    BASE_URL: 'http://localhost:3004',
+    PUBLISH_KEY: '',
+    SUBSCRIBE_KEY: '',
+    CHAT_ROOM: '7f1a3734-a063-4fdc-3d23-169c22d08b6b',
+    SOCKET_ENDPOINT: 'http://localhost:3000',
+    API_URL: 'http://localhost:3001/',
+  };
+  
---
+++ services/authentication/src/repositories/index.ts
@@ -0,0 +1,3 @@
+export * from './role.repository';
+export * from './tenant.repository';
+export * from './user.repository';
